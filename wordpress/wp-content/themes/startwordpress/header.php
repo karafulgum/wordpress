c2e7805b49ee7428ec1a886f94e4c452c311ab84
Kara Fulgum <karafulgum@gmail.com>
2017-01-30 17:04:33 -0600

    Add functions.php

--- wordpress/wp-content/themes/startwordpress/header.php
+++ wordpress/wp-content/themes/startwordpress/header.php
@@ -8,13 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?php echo get_bloginfo( 'name' ); ?></title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
