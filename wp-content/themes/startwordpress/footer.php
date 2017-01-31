14e8fe9dc333f4c1b5aa8a4e5e4c2054759b1f8f
Kara Fulgum <karafulgum@gmail.com>
2017-01-31 16:11:17 -0600

    Add functions.php

--- wp-content/themes/startwordpress/footer.php
+++ wp-content/themes/startwordpress/footer.php
@@ -1,5 +1,5 @@
+</div> <!-- /.container -->
 
-</div><!-- /.container -->
 <footer class="blog-footer">
   <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
   <p>
@@ -7,10 +7,6 @@
   </p>
 </footer>
 
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <?php wp_footer(); ?>
 </body>
 </html>
